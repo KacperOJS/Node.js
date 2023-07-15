850fa8d54cfb8447937e54c750fec4803286545f
Kacper <kacperodziemczyk@gmail.com>
2023-07-15 19:33:01 +0200

    make listen file index.html

--- app.js
+++ app.js
@@ -1,12 +1,13 @@
-const EventEmitter = require('events')
-
-const customEmitter = new EventEmitter()
-
-
-customEmitter.on('response',()=>{
-  console.log(`data recieved`);
+const express = require('express')
+const path =require('path')
+const app = express();
+app.get('/',(req,res)=>{
+res.sendFile(path.resolve(__dirname,'./index.html'))
 })
-customEmitter.on('response',()=>{
-  console.log(`some logic `);
+app.all('*',(req,res)=>{
+	res.status(404).send('resource not found')
 })
-customEmitter.emit('response')
+
+app.listen(5000,()=>{
+	console.log('server is listening on port 5000...');	
+})
