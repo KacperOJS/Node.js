33b7ef848d4a0efaa0c1739fe284381d24600216
Kacper <kacperodziemczyk@gmail.com>
2023-08-21 15:20:36 +0200

    make logout and clear jwttoken

--- mvc/Controllers/refreshTokenController.js
+++ mvc/Controllers/refreshTokenController.js
@@ -1,36 +1,27 @@
-const usersDB = {
-	users: require('../model/users.json'),
-	setUsers: function(data) {
-	  this.users = data;
-	}
-  };
-  const jwt = require('jsonwebtoken');
-  require('dotenv').config();
+const jwt = require('jsonwebtoken');
+const bcrypt = require('bcrypt');
+const { users } = require('../model/users.json'); // Assuming 'users.json' contains an array of user objects.
+require('dotenv').config();
 
+const handleRefreshToken = (req, res) => {
+  const cookies = req.cookies; // Use 'req.cookies' instead of 'req.cookie'
+  if (!cookies?.jwt) return res.sendStatus(401);
 
-  const handleRefreshToken = (req,res)=>{
-	const cookies=req.cookie;
-	if(!cookies?.jwt) return res.sendStatus(401)
-	console.log(cookies.jwt);
-	const refreshToken = cookies.jwt;
-	const foundUser = usersDB.users.find((person)=>person.refreshToken ===refreshToken)
-	if(!foundUser) return res.sendStatus(403); //forbidden
-	//evaluate password
-	// const match = await bcrypt.compare(pwd,foundUser.password);
-	jwt.verify(
-		refreshToken,
-		process.env.REFRESH_TOKEN_SECRET,
-		(err,decoded)=>{
-			if(err || foundUser.username !== decoded.username) return res.sendStatus(403) //forbidden
-			const accessToken = jwt.sign(
-				{"username": decoded.username},
-				process.env,ACCESS_TOKEN_SECRET,
-				{expiresIn: '30s'}
-			);
-			res.json({accessToken})
-		}
-	)
+  const refreshToken = cookies.jwt;
+  const foundUser = users.find((person) => person.refreshToken === refreshToken);
+  if (!foundUser) return res.sendStatus(403); // Forbidden
 
-  }
+  jwt.verify(refreshToken, process.env.REFRESH_TOKEN_SECRET, (err, decoded) => {
+    if (err || foundUser.username !== decoded.username) return res.sendStatus(403); // Forbidden
 
-  module.exports = {handleRefreshToken}
+    const accessToken = jwt.sign(
+      { username: decoded.username },
+      process.env.ACCESS_TOKEN_SECRET, 
+      { expiresIn: '30s' }
+    );
+
+    res.json({ accessToken });
+  });
+};
+
+module.exports = { handleRefreshToken };
