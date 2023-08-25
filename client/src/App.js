b0ec253b10179b874ab6e9cd2030efe4353482bf
Kacper <kacperodziemczyk@gmail.com>
2023-08-25 17:05:00 +0200

    react query  tut

--- client/src/App.js
+++ client/src/App.js
@@ -1,58 +1,27 @@
-import React, { useState, useEffect } from 'react';
-import axios from 'axios';
+import React from 'react';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { BrowserRouter, Routes, Route } from 'react-router-dom'; // Update the import statement
+import Home from './components/Home';
+import Profile from './components/Profile';
+import Contact from './components/Contact';
 
 const App = () => {
-    const [listOfUsers, setListOfUsers] = useState([]);
-    const [formData, setFormData] = useState({
-        name: '',
-        age: 0,
-        username: ''
-    });
+  const client = new QueryClient();
 
-    useEffect(() => {
-        axios.get("http://localhost:3003/getUsers")
-            .then((response) => {
-                setListOfUsers(response.data);
-            });
-    }, [listOfUsers]);
-
-    const createUser = () => {
-        axios.post("http://localhost:3003/createUser", formData)
-            .then((response) => {
-				alert('user created')
-                // Do something with the response if needed
-            });
-    };
-
-    const handleInputChange = (e) => {
-        const { name, value } = e.target;
-        setFormData(prevData => ({
-            ...prevData,
-            [name]: value
-        }));
-    };
-
-    return (
-        <>
-            <div>
-                {listOfUsers.map((user, index) => (
-                    <div key={index}>
-                        <li>Name: {user.name}</li>
-                        <li>Age: {user.age}</li>
-                        <li>Username: {user.username}</li>
-                    </div>
-                ))}
-            </div>
-            <div>
-                <br /><br />
-                Create User<br />
-                Name: <input type='text' name='name' onChange={handleInputChange} /><br />
-                Age: <input type='number' name='age' onChange={handleInputChange} /><br />
-                Username: <input type='text' name='username' onChange={handleInputChange} />
-                <button onClick={createUser}>Create</button>
-            </div>
-        </>
-    );
+  return (
+    <div className='App'>
+      <QueryClientProvider client={client}>
+        <BrowserRouter> {/* Use BrowserRouter here */}
+          <Routes>
+            <Route path='/' element={<Home />} />
+            <Route path='/profile' element={<Profile />} />
+            <Route path='/contact' element={<Contact />} />
+            <Route path='*' element={<h1>404 Page not found</h1>} />
+          </Routes>
+        </BrowserRouter>
+      </QueryClientProvider>
+    </div>
+  );
 };
 
 export default App;
