6fa9bf1424c75d48d22a5bca3418560db559dc11
Kacper <kacperodziemczyk@gmail.com>
2023-08-27 14:47:28 +0200

    Edited schemas and routes section

--- server/models/Note.js
+++ server/models/Note.js
@@ -1,31 +1,35 @@
-const mongoose = require('mongoose');
-const AutoIncreament = require('mongoose-sequence')(mongoose)
-const noteSchema = new Scheam({
-	user:{ 
-		type: mongoose.Schema.Types.ObcjectId,
-		required:true,
-		ref: 'User'
-	},
-	title:{
-		type:String,
-		required:true
-	},
-	text:{
-		type:String,
-		required:true
-	},
-	active:{
-		type:Boolean,
-		default:false	
-	}
-},
-{
-	timestamps:true
-}
+const mongoose = require('mongoose')
+const AutoIncrement = require('mongoose-sequence')(mongoose)
+
+const noteSchema = new mongoose.Schema(
+    {
+        user: {
+            type: mongoose.Schema.Types.ObjectId,
+            required: true,
+            ref: 'User'
+        },
+        title: {
+            type: String,
+            required: true
+        },
+        text: {
+            type: String,
+            required: true
+        },
+        completed: {
+            type: Boolean,
+            default: false
+        }
+    },
+    {
+        timestamps: true
+    }
 )
-noteSchema.plugin(AutoIncreament,{
-	inc_field:'ticket',
-	id:'ticketNums',
-	start_seq: 500
+
+noteSchema.plugin(AutoIncrement, {
+    inc_field: 'ticket',
+    id: 'ticketNums',
+    start_seq: 500
 })
-module.exports = mongoose.model('Note',noteSchema)
+
+module.exports = mongoose.model('Note', noteSchema)
--- server/server.js
+++ server/server.js
@@ -1,47 +1,53 @@
-const express = require('express');
-const app = express();
-require('dotenv').config();
-const PORT = process.env.PORT || 3500;
-const path = require('path');
-const cookieParser = require('cookie-parser');
-const errorHandler = require('./middleware/errorHandler');
-const cors = require('cors');
-const corsOptions = require('./config/corsOptions');
-const connectDB = require('./config/dbConn');
-const mongoose = require('mongoose');
-const { logger,logEvents } = require('./middleware/logger');
+require('dotenv').config()
+const express = require('express')
+const app = express()
+const path = require('path')
+const { logger, logEvents } = require('./middleware/logger')
+const errorHandler = require('./middleware/errorHandler')
+const cookieParser = require('cookie-parser')
+const cors = require('cors')
+const corsOptions = require('./config/corsOptions')
+const connectDB = require('./config/dbConn')
+const mongoose = require('mongoose')
+const PORT = process.env.PORT || 3500
 
-connectDB();
+console.log(process.env.NODE_ENV)
 
-app.use(logger);
+connectDB()
 
-app.use(express.json());
+app.use(logger)
 
-app.use(cors(corsOptions));
+app.use(cors(corsOptions))
 
-app.use(cookieParser());
+app.use(express.json())
 
-app.use('/',express.static(path.join(__dirname, '/public')))
+app.use(cookieParser())
 
-app.use('/',require('./routes/root'))
+app.use('/', express.static(path.join(__dirname, 'public')))
 
-app.all('*',(req,res)=>{
-	res.status(404);
-	if(req.accepts('html')){
-		res.sendFile(path.join(__dirname,'views','404.html'))
-	}else if(req.accepts('json')){
-		res.json({message:"404 Not Found"})
-	}else {
-		res.type('txt').send('404 Not Found');
-	}
+app.use('/', require('./routes/root'))
+app.use('/users', require('./routes/userRoutes'))
+app.use('/notes', require('./routes/noteRoutes'))
+
+app.all('*', (req, res) => {
+    res.status(404)
+    if (req.accepts('html')) {
+        res.sendFile(path.join(__dirname, 'views', '404.html'))
+    } else if (req.accepts('json')) {
+        res.json({ message: '404 Not Found' })
+    } else {
+        res.type('txt').send('404 Not Found')
+    }
 })
-app.use(errorHandler);
 
-mongoose.connection.once('open',()=>{
-	console.log('Connected to MongoDB')
-	app.listen(PORT, ()=>console.log(`Server is running on port ${PORT}`))
+app.use(errorHandler)
+
+mongoose.connection.once('open', () => {
+    console.log('Connected to MongoDB')
+    app.listen(PORT, () => console.log(`Server running on port ${PORT}`))
 })
-mongoose.connection.on('error',err=>{
-	console.log(err);
-	logEvents(`${err.no}: ${err.code} ${err.syscall} \t ${err.hostname}`,'mongoErrLog.log')
+
+mongoose.connection.on('error', err => {
+    console.log(err)
+    logEvents(`${err.no}: ${err.code}\t${err.syscall}\t${err.hostname}`, 'mongoErrLog.log')
 })
