f47d5bfdca5cf9c1c35c0d8d0812c54b5262cc03
Kacper <kacperodziemczyk@gmail.com>
2023-08-28 15:36:10 +0200

    Make some changes context and Welcome Page

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,17 +1,22 @@
-import { useState,useEffect } from 'react'
-
-import './App.css'
-
-function App() {
-  const [count, setCount] = useState(0)
-  useEffect(()=>{
-	console.log('dasdasda');
-  },[])
+import React from 'react'
+import {Routes,Route} from 'react-router-dom';
+import Layout from './components/Layout';
+import Public from './components/Public';
+import Login from './features/Login';
+import DashLayout from './components/DashLayout';
+import Welcome from './features/Welcome';
+const App = () => {
   return (
-    <>
+	<Routes>
+		<Route path='/' element={<Layout/>}>
+			<Route index element={<Public/>}/>
+			<Route path="login" element={<Login/>}/>
 
-       
-    </>
+			<Route path="dash" element={<DashLayout/>}/>
+
+			<Route index element={<Welcome/>}/>
+		</Route>
+	</Routes>
   )
 }
 
