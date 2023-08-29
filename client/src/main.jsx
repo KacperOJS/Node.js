978905b2fa4369a6963c1c044f528909200b2da6
Kacper <kacperodziemczyk@gmail.com>
2023-08-29 14:23:29 +0200

    Redux CreateApi

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -2,15 +2,18 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import { BrowserRouter,Routes,Route } from 'react-router-dom'
-
+import { store } from './app/store.jsx'
+import { Provider } from 'react-redux'
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
+	<Provider store ={store}>
 	<BrowserRouter>
 		<Routes>
 			<Route path ='/*' element = {<App/>}/>
 			
 		</Routes>
 	</BrowserRouter>
+	</Provider>
     {/* <App /> */}
   </React.StrictMode>,
 )
