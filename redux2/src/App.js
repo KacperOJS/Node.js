e7463ff3712a55c8e4099056974dfe57215d3e02
Kacper <kacperodziemczyk@gmail.com>
2023-08-30 13:31:09 +0200

    understanding redux

--- redux2/src/App.js
+++ redux2/src/App.js
@@ -1,12 +1,23 @@
-import {useSelector} from "react-redux";
-
+import {useDispatch, useSelector} from "react-redux";
+import { addUser } from "./features/Users";
+import { useState } from "react";
+// import { uuid } from 'uuidv4';
 const App = () => {
+	const dispatch = useDispatch();
 	const userList = useSelector((state)=> state.users.value)
+	const [name,setName] = useState(" ");
+	const [username,setUsername] = useState(" ");
   return (
 	<div>
-		App
-		{userList.map((user)=>{
-			return <h1>{user.name}</h1>
+
+		<input type="text" placeholder="Name..." onChange={(e)=> setName(e.target.value)}/>
+		<input type="text" placeholder="Surname.." onChange={(e)=> setUsername(e.target.value)}/>
+		<button onClick={()=>{dispatch(addUser({id:userList[userList.length - 1].id + 1,name:name ,surname:username}))}}>Add user</button>
+		{userList.map((user,idx)=>{
+			return <div key={idx}>
+			<h1>{user.name}</h1>
+			<h1>{user.surname}</h1>
+			</div>
 		})}
 	</div>
   )
--- redux2/src/features/Users.js
+++ redux2/src/features/Users.js
@@ -5,9 +5,10 @@ export const UserSlice = createSlice({
 	initialState:{value:UsersDB},
 	reducers:{
 		addUser:(state,action)=>{
-
+			state.value.push(action.payload);
 		}
 	}
-})
+});
 
+export const {addUser} = UserSlice.actions;
 export default UserSlice.reducer;
