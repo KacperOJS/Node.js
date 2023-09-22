b5a40920104e6c75fd7fa55ae7dc61f31b517f5a
Kacper <kacperodziemczyk@gmail.com>
2023-09-22 22:24:00 +0200

    reminder task

--- training/index.js
+++ training/index.js
@@ -1,29 +1,41 @@
 const express = require('express');
 const app = express();
-PORT = process.env.PORT || 3333;
+const PORT = process.env.PORT || 3333;
+const fsPromises = require("fs").promises;
+const path = require("path"); // Import the 'path' module
 
-const array = [
-	[1, 2, 3, 4, 5, 6],
-	[10, 12, 14, 16, 18, 20],
-	[100, 120, 140, 160, 180, 200],
-  ];
-  
-  let sum = 0;
-  
-  for (let i = 0; i < array.length; i++) {
-	for (let j = 0; j < array[i].length; j++) {
-	  console.log(array[i][j]);  // Access the correct element using array[i][j]
-	  sum += array[i][j];
-	}
+// Add middleware to parse JSON data
+app.use(express.json());
+
+const UsersDB = {
+  users: require("./users.json"),
+  setUsers: function (data) {
+    this.users = data;
+  },
+};
+
+app.get('/', (req, res) => {
+  res.send('witaj');
+});
+
+app.post('/createuser', async (req, res) => {
+  const { username, password } = req.body;
+  if (!username || !password) return res.status(400).json({ 'message': 'Required to write in inputs' });
+
+  const checkDuplicated = UsersDB.users.find((person) => person.username === username || person.password === password);
+  if (checkDuplicated) return res.status(400).json({ 'message': `In the database, there is already an existing name ${username}` });
+
+  try {
+    const newUser = { "username": username, "password": password };
+    UsersDB.setUsers([...UsersDB.users, newUser]);
+    await fsPromises.writeFile(path.join(__dirname, 'users.json'), JSON.stringify(UsersDB.users, null, 2));
+  } catch (err) {
+    console.error(err);
   }
-  
-  console.log(sum);
-  
 
-app.get('/',(req,res)=>{
-	res.send('witaj')
-})
+  res.status(201).json({ 'success': `Created a new user with name ${username}` });
+});
 
-app.listen(PORT,()=>{
-	console.log(`Server is running on port ${PORT}`);
-})
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
